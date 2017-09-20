538ca4d64f8a819d959e2b7a69681a3298b50afe
dinal herath <dinal.bing@gmail.com>
2017-09-20 15:51:28 -0400

    created submodule for A2

---
+++ jerome_herath_assign_2/studentCoursesBackup/src/studentCoursesBackup/driver/Driver.java
@@ -0,0 +1,12 @@
+package studentCoursesBackup.driver;
+
+public class Driver 
+{
+
+	public static void main(String[] args) 
+	{
+	    // command line validation is missing here. FIXME!
+	    System.out.println("Hello World: " + args[0] + ", " + args[1] + ", " + args[2]+", "+args[3]+", "+args[4]);
+	}
+	
+}
